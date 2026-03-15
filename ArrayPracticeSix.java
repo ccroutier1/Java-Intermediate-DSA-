666efe1254dde528837107e6b6e82baa0401925d
Rover James <114409097+ccroutier1@users.noreply.github.com>
2026-03-15 23:27:24 +0800

    Add files via upload

--- ArrayPracticeSix.java
+++ ArrayPracticeSix.java
@@ -1,33 +1,24 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
+public class ArrayPracticeSix {
 
-//Neighbor Swap using 1D ARRAY
-
-public class  ArrayPracticeSix {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        String[] foods;
+        int size;
 
-        System.out.print("How many number you want to input? ");
+        System.out.print("How many food you want to enter: ");
+        size = input.nextInt();
+        foods = new String[size];
 
-            int arraySize = input.nextInt();
-
-            int arrayNum[] = new int[arraySize];
-
-        for(int i = 0; i < arrayNum.length ; i++)
-        {
-            System.out.print("Enter a number: ");
-                      arrayNum[i] = input.nextInt();
+        for(int i = 0; i < foods.length; i++) {
+            System.out.print("Enter a food: ");
+            foods[i] = input.nextLine();
         }
-        for(int i = 0; i < arrayNum.length - 1; i+=2)
-        {
-            int num = arrayNum[i + 1];
-                      arrayNum[i + 1] =arrayNum[i];
-                      arrayNum[i] = num;
-        }
-        for(int i : arrayNum)
-        {
-            System.out.print(i + "");
+        for(String food : foods) {
+            System.out.println(food);
         }
     }
 }
